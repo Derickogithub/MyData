690b1ce9b2b604287d3a52e16862ff7b4c00bb89
Derickogithub <okwaderick@gmail.com>
2022-06-25 09:57:24 +0100

    Basic info about me

---
+++ app.js
@@ -0,0 +1,6 @@
+const myData = {
+    Name: "Derick Okwa",
+    Height: "6.6ft tall",
+    Country: "Nigeria"
+};
+document.getElementById("root").innerHTML = myData.Name + "," + "" + "" + myData.Height + "," + myData.Country
